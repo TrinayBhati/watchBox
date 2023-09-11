b100f10eacfed2887e5777e02aa4b19a33af8897
Trinay Bhati <72141578+TrinayBhati@users.noreply.github.com>
2023-09-11 16:16:57 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,19 +1,43 @@
 import axios from "axios";
 
-const BASE_URL = "http://api.themoviedb.org/3";
-const TMDB_TOKEN =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjN2MzNTQzNjJhOGJlYTE5N2JiYTYyOTI5MzA5NWQ2YiIsInN1YiI6IjY0OTBhODNiOTc2YTIzMDExZTFiZDdlNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wB7IVwR5XsJ7YwEq7oao7PlP_Epg-CMvGJay-hMeeDk";
+const BASE_URL = "https://api.themoviedb.org/3";
+const TMDB_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYzBhM2U3MjgwZTlhMmY2Y2FjMzgzYzRkZWE3OGM3ZSIsInN1YiI6IjY0ZTRhYTY2NTk0Yzk0MDBmZmU0NDFiMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HiPRsYVJz2CEOucUQXSTakU6hC5YK0S8bASq23L1DUY";
 
 const headers = {
-  Authorization: "bearer " + TMDB_TOKEN,
+    Authorization: "bearer " + TMDB_TOKEN,
 };
 
 export const fetchDataFromApi = async (url, params) => {
-  try {
-    const { data } = await axios.get(BASE_URL + url, { headers, params });
-    return data;
-  } catch (err) {
-    console.log(err);
-    return err;
-  }
-};
+    try {
+        const { data } = await axios.get
+            (BASE_URL + url, {
+                headers,
+                params
+            })
+        // const data = data1.data;
+        return data;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+}
+
+
+
+// DESTRUCTURE OF DATA (BELOW CODE WITHOUT DESTRUCRING)
+
+// axios.get(BASE_URL + url, { headers, params })
+//     .then(function (response) {
+//         const data = response.data;
+//         // your code here
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     });
+
+// destrucued data coz n =in response we have alotof things and in that thwere is jey named data in whihch we have an array of 20 obj
+// so we destructred that data likewe do if we have one object with difefernt keys so we write key name  =  object
+// and destrucred it
+
+//data here and in useFecth are differnt as name is same but differnet varibales hrer data is havng response
+// get from api and there data is a state in whihc we updating(storing) the response
